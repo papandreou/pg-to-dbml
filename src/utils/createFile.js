8a91a2df82bf6d3c956951653f5662987167898e
Jeff L. Bumgardner <jbumgardner@polarislabs.com>
2020-12-15 12:37:28 -0500

    small clean up

--- src/utils/createFile.js
+++ src/utils/createFile.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const { constants, fs } = require('fs');
 
 module.exports = (fileName) => {
-  if (fs.existsSync(fileName, fs.constants.R_OK | fs.constants.W_OK)) {
+  if (fs.existsSync(fileName, constants.R_OK | constants.W_OK)) {
     fs.writeFileSync(fileName, '', () => { });
   }
 }
