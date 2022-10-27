4f5f6289477ed3f57009217f4311994e276102ba
Andreas Lind <andreas.lind@workday.com>
2022-10-27 22:58:35 +0200

    Don't quote the schema name in Enum definitions either (for consistency)

--- src/funcs/transformEnumToDBML.js
+++ src/funcs/transformEnumToDBML.js
@@ -7,9 +7,9 @@ module.exports = function transformEnumToDBML(
 ) {
   // It seems like dbdocs.io doesn't support a comment on the enum itself, so the comment isn't used for now
 
-  const enumNameString = includeSchemaName ? `${schemaName}.${name}` : name;
+  const enumNameString = includeSchemaName ? `${schemaName}."${name}"` : `"${name}"`;
 
-  return `Enum "${enumNameString}" {${EOL}\t${values
+  return `Enum ${enumNameString} {${EOL}\t${values
     .map(value => `"${value}"`)
     .join(`${EOL}\t`)}${EOL}}${EOL}${EOL}`;
 };
